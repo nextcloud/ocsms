a380ac71ff5a7989486bb668566cc291f1e81d9c
Loïc Blot (@U-Exp) <loic.blot@unix-experience.fr>
2014-09-12 14:22:02 +0200

    Syntax fix

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@
 		$('#echo').click(function () {
 			var url = OC.generateUrl('/apps/ocsms/push');
 			var data = {
-				sms_count: 1
+				sms_count: 1,
 				sms_datas: {
 					{"read": 1, "date": 1410524385, "seen": 0, "address": "+33612121212", "body": "testSMS", "_id": 10},
 					{"read": 0, "date": 1400524385, "seen": 1, "address": "+33614121212", "body": "test SMS 2", "_id": 14},
