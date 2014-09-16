b559b439830d6386e9608ed15ea5ee30d134e52b
Loic Blot <loic.blot@unix-experience.fr>
2014-09-16 19:14:32 +0000

    Add style for mailbox-content

--- templates/part.navigation.php
+++ templates/part.navigation.php
@@ -1,12 +1,7 @@
 <ul>
 	<?php foreach ($_['mailboxes'] as $mboxName => $mailbox) { ?>
 		<?php if (count($mailbox['phoneNumbers']) > 0) { ?>
-		<li><a href="<?php p($mailbox['url']); ?>"><?php p($mailbox['label']); ?></a>
-		<ul>
-		<?php foreach ($mailbox['phoneNumbers'] as $number) { ?>
-			<li><?php p($number); ?></li>
-		<?php } ?>
-		</ul></li>
-		<?php }
+		<li><a href="<?php p($mailbox['url']); ?>"><?php p($mailbox['label']); ?></a></li>
+	<?php }
 	} ?>
 </ul>
