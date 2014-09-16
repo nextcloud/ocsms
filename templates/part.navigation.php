431796c8beb7f232c9a978c0165a98c90127977c
Loic Blot <loic.blot@unix-experience.fr>
2014-09-16 17:34:42 +0200

    Prepare template navigation view for conversations

--- templates/part.navigation.php
+++ templates/part.navigation.php
@@ -1,4 +1,19 @@
 <ul>
 	<li><a href="#">Conversations</a></li>
+	<?php if count($_["PNLConversations"]) > 0) { ?>
+	<ul>
+	<?php foreach ($_["PNLConversations"] as $number) { ?>
+		<li><?php p($number); ?></li>
+	<?php } ?>
+	</ul>
+	<?php }	?>
+	
 	<li><a href="#">Drafts</a></li>
+	<?php if count($_["PNLDrafts"]) > 0) { ?>
+	<ul>
+	<?php foreach ($_["PNLDrafts"] as $number) { ?>
+		<li><?php p($number); ?></li>
+	<?php } ?>
+	</ul>
+	<?php }	?>
 </ul>
