a438a826816e844d33031d698185dbe4f686b5d6
Loïc Blot (@U-Exp) <loic.blot@unix-experience.fr>
2015-01-03 11:12:09 +0100

    Add missing copyright

--- db/sms.php
+++ db/sms.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * ownCloud - ocsms
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Loic Blot <loic.blot@unix-experience.fr>
+ * @copyright Loic Blot 2014-2015
+ */
+
 namespace OCA\OcSms\Db;
 
 use \OCP\AppFramework\Db\Entity;
