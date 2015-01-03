ab3bd8718eba817bba14c6bddc8fef3ea9374bbc
Loïc Blot (@U-Exp) <loic.blot@unix-experience.fr>
2015-01-03 11:59:25 +0100

    Add a basic countrycode mapper for future use to dedup phone intl/local prefix problems

---
+++ lib/countrycodes.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * ownCloud - ocsms
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Loic Blot <loic.blot@unix-experience.fr>
+ * @contributor: stagprom <https://github.com/stagprom/>
+ * @copyright Loic Blot 2014-2015
+ */
+
+namespace OCA\OcSms\Lib;
+
+class CountryCodes {
+	public static $codes = array(
+		"Algeria" => "+213",
+		"Belgium" => "+32",
+		"Brazil" => "+55",
+		"Canada" => "+1",
+		"Egypt" => "+20",
+		"France" => "+33",
+		"Germany" => "+49",
+		"Hong Kong" => "+852",
+		"India" => "+91",
+		"Italy" => "+39",
+		"Japan" => "+81",
+		"Mexico" => "+52",
+		"Morocco" => "+212",
+		"Norway" => "+47",
+		"Poland" => "+48",
+		"Russia" => "+7",
+		"South Korea" => "+82",
+		"South Sudan" => "+211",
+		"Spain" => "+34",
+		"Sweden" => "+46",
+		"Tunisia" => "+216",
+		"USA" => "+1",
+
+	);
+};
