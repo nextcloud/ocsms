7a4f37f585809fd77dc25d27afde5c9d82981da9
Loic Blot <loic.blot@unix-experience.fr>
2016-05-19 22:07:37 +0200

    Little CSS enhancement and bind peer ng-click on li, not a

--- templates/main.php
+++ templates/main.php
@@ -9,10 +9,10 @@ use \OCA\OcSms\Lib\CountryCodes;
 <div class="ng-scope" id="app" ng-app="OcSms" ng-controller="OcSmsController">
 	<div id="app-mailbox-peers">
 		<ul class="contact-list">
-			<li ng-repeat="contact in contacts" peer-label="{{ contact.label }}">
+			<li ng-repeat="contact in contacts" peer-label="{{ contact.label }}" ng-click="loadConversation(contact);" href="#">
 				<div class="ocsms-plavatar" style="background-image: url('{{ contact.avatar }}');" ng-show="contact.avatar !== undefined"></div>
 				<div class="ocsms-plavatar" ng-show="contact.avatar === undefined" ng-style="{'background-color': (contact.label | peerColor)}">{{ contact.label | firstCharacter }}</div>
-				<a class="ocsms-plname" style="{{ contact.unread > 0 ? 'font-weight:bold;' : ''}}" mailbox-label="{{ contact.label }}" mailbox-navigation="{{ contact.nav }}" ng-click="loadConversation(contact);" href="#">{{ contact.label }}{{ contact.unread > 0 ? ' (' + contact.unread + ') ' : '' }}</a>
+				<a class="ocsms-plname" style="{{ contact.unread > 0 ? 'font-weight:bold;' : ''}}" mailbox-label="{{ contact.label }}" mailbox-navigation="{{ contact.nav }}">{{ contact.label }}{{ contact.unread > 0 ? ' (' + contact.unread + ') ' : '' }}</a>
 			</li>
 		</ul>
 	</div>
