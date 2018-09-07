nextcloud/ocsms

commit 580e4415e24f3a7496f930aac9a2386148a56ebf

Fix the unread state (#251)
The checkNewMessages method was using an old regex phone number formatter and not returning the phone numbers in a formated fashion back to the client side.

controller/smscontroller.php
@@ -221,6 +221,7 @@ class SmsController extends Controller {
 	 */
 	public function checkNewMessages($lastDate) {
 		$phoneList = $this->smsMapper->getNewMessagesCountForAllPhonesNumbers($this->userId, $lastDate);
+		$formatedPhoneList = array();
 		$contactsSrc = $this->contactCache->getContacts();
 		$photosSrc = $this->contactCache->getContactPhotos();
 		$uidsSrc = $this->contactCache->getContactUids();
@@ -228,8 +229,12 @@ class SmsController extends Controller {
 		$photos = array();
 		$uids = array();
 
+		// Cache country because of loops
+		$configuredCountry = $this->configMapper->getCountry();
+
 		foreach ($phoneList as $number => $ts) {
-			$fmtPN = preg_replace("#[ ]#","", $number);
+			$fmtPN = PhoneNumberFormatter::format($configuredCountry, $number);
+			$formatedPhoneList[] = $fmtPN;
 			if (isset($contactsSrc[$fmtPN])) {
 				$contacts[$fmtPN] = $contactsSrc[$fmtPN];
 				if (isset($uidsSrc[$fmtPN])) {
@@ -242,7 +247,7 @@ class SmsController extends Controller {
 			}
 		}
 
-		return new JSONResponse(array("phonelist" => $phoneList, "contacts" => $contacts, "photos" => $photos, "uids" => $uids));
+		return new JSONResponse(array("phonelist" => $formatedPhoneList, "contacts" => $contacts, "photos" => $photos, "uids" => $uids));
 	}
 
 	/**

commit ea964c015fe2c686ac8cd2bdf22ea5ae338d2588

Fix wrong message count when checking new messages
Regression was introduced in commit: 580e4415e24f3a7496f930aac9a2386148a56ebf

controller/smscontroller.php
@@ -235,7 +235,7 @@ class SmsController extends Controller {
 
 		foreach ($phoneList as $number => $ts) {
 			$fmtPN = PhoneNumberFormatter::format($configuredCountry, $number);
-			$formatedPhoneList[] = $fmtPN;
+			$formatedPhoneList[$number] = $ts;
 			if (isset($contactsSrc[$fmtPN])) {
 				$contacts[$fmtPN] = $contactsSrc[$fmtPN];
 				if (isset($uidsSrc[$fmtPN])) {
@@ -248,7 +248,7 @@ class SmsController extends Controller {
 			}
 		}
 
-		return new JSONResponse(array("phonelist" => $formatedPhoneList, "contacts" => $contacts, "photos" => $photos, "uids" => $uids));
+		return new JSONResponse(array("phonelist" => $phoneList, "contacts" => $contacts, "photos" => $photos, "uids" => $uids));
 	}
 
 	/**