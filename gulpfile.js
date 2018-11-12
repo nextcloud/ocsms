dc06bd8f8830a91489b5815b107fe623c735d8b2
Loic Blot <loic.blot@unix-experience.fr>
2018-11-12 14:15:50 +0100

    Fix build another time

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,18 @@ let concat = require('gulp-concat');
 let rename = require("gulp-rename");
 let uglify = require('gulp-uglify-es').default;
 
+js_files = [
+	'js/devel/app.js',
+	'js/devel/settings.js',
+	'js/devel/contactlist.js',
+	'js/devel/conversation.js',
+	'js/devel/helpers.js',
+	'js/devel/notifications.js'
+];
+
 gulp.task("uglify", function () {
-	return gulp.src("js/devel/app.js")
-		.pipe(concat('js/devel/settings.js'))
-		.pipe(concat('js/devel/contactlist.js'))
-		.pipe(concat('js/devel/conversation.js'))
-		.pipe(concat('js/devel/helpers.js'))
-		.pipe(concat('js/devel/notifications.js'))
-		.pipe(rename("app.min.js"))
+	return gulp.src(js_files)
+		.pipe(concat("app.min.js"))
 		.pipe(uglify(/* options */))
 		.pipe(gulp.dest("js/"));
 });
