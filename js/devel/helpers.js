b01f542c91d65ebc71bb8d843272b154c3d8e4b1
Alfred Egger <alfred.egger@gmx.at>
2020-12-16 21:13:40 +0100

    Add escapeHTML function to Javascipt code

--- js/devel/helpers.js
+++ js/devel/helpers.js
@@ -27,3 +27,52 @@ function toBool(str) {
 	}
 	return null;
 }
+
+
+function escapeHTML(string) {
+                var str = '' + string
+                var matchHtmlRegExp = /["'&<>]/
+                var match = matchHtmlRegExp.exec(str)
+
+                if (!match) {
+                        return str
+                }
+
+                var escape
+                var html = ''
+                var index = 0
+                var lastIndex = 0
+
+                for (index = match.index; index < str.length; index++) {
+                        switch (str.charCodeAt(index)) {
+                                case 34: // "
+                                        escape = '&quot;'
+                                        break
+                                case 38: // &
+                                        escape = '&amp;'
+                                        break
+                                case 39: // '
+                                        escape = '&#39;'
+                                        break
+                                case 60: // <
+                                        escape = '&lt;'
+                                        break
+                                case 62: // >
+                                        escape = '&gt;'
+                                        break
+                                default:
+                                        continue
+                        }
+
+                        if (lastIndex !== index) {
+                                html += str.substring(lastIndex, index)
+                        }
+
+                        lastIndex = index + 1
+                        html += escape
+                }
+
+                return lastIndex !== index
+                        ? html + str.substring(lastIndex, index)
+                        : html
+}
